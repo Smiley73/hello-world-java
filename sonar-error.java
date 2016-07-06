b93f439893f6c8f3ece454c0b24bd008ae05bc4e
Smiley73 <heiko@zuerker.org>
2016-07-05 21:25:26 -0500

    Update sonar-error.java

--- sonar-error.java
+++ sonar-error.java
@@ -28,19 +28,17 @@ public class ComplexMethod {
 public enum Continent {
 
   NORTH_AMERICA (23, 24709000),
-  // ...
   EUROPE (50, 39310000);
 
   public int countryCount;  // Noncompliant
   private int landMass;
 
   Continent(int countryCount, int landMass) { 
-    // ...
+    return null;
   }
 
   public void setLandMass(int landMass) {  // Noncompliant
     this.landMass = landMass;
   }
   
-
 }
